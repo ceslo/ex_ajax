95a3af448ecc224eff253870fc8395d9180c099c
ceslo <slovesce@gmail.com>
2023-11-10 11:57:43 +0100

    essai modif hauteur image a retravailler

--- ex_3.js
+++ ex_3.js
@@ -9,16 +9,16 @@ $(document).ready(function () {
 
     $.getJSON(api, function (data) {
       // console.log(data.results)
-
+      $("#aff_res div").remove();
       $.each(data.results, function (index, film) {
         $("#aff_res").append(
-          '<div class="card m-3 col-lg-2 col-sm-3 shadow-lg rounded" ><div class="card-body gy-0 d-flex row"> <div class="card-title text-center align-items-start  p-3">' +
+          '<div class="card m-3 col-lg-2 col-sm-3 shadow-lg rounded h-25" ><div class="card-body gy-0 d-flex row"> <div class="card-title text-center align-items-start  p-3">' +
             film.title +
             '</div>  <div class="align-self-end"> <div class="card-text">Date de sortie: ' +
             film.release_date +
             '</div><div class="card-text"> Note des utilisateurs: '+
             film.vote_average +
-            '</div></div></div><img class="card-img-bottom" src="http://image.tmdb.org/t/p/w185' +
+            '</div></div></div><img class="card-img-bottom object-fit-contain img-fluid"style=height:250px src="http://image.tmdb.org/t/p/w185' +
             film.poster_path +
             '"></div>'
         );
